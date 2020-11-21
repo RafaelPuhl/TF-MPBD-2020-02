ce27dd0195ab117049b1ff6a804f52a5ec8c06c2
jonathanCB <jonas.ca2@gmail.com>
2020-11-21 17:55:48 -0300

    Adicionando inserts de Abrigos e Adotantes

---
+++ Inserts/Inserts-Abrigo-e-Adotante.sql
@@ -0,0 +1,50 @@
+select * from BJ204236.abrigo;
+
+INSERT INTO BJ204236.abrigo
+VALUES(1,'Abrigo 1', 123, 'Rua Cavalcante', 'Porto Alegre','RS');
+
+INSERT INTO BJ204236.abrigo
+VALUES(2,'Abrigo 2', 456, 'Rua Andarilho', 'Canoas','RS');
+
+INSERT INTO BJ204236.abrigo
+VALUES(3,'Abrigo 3', 789, 'Rua Flores da Cunha', 'Alvorada','RS');
+
+INSERT INTO BJ204236.abrigo
+VALUES(4,'Abrigo 4', 112, 'Rua Manoel Arruda', 'Porto Alegre','RS');
+
+INSERT INTO BJ204236.abrigo
+VALUES(5,'Abrigo 5', 2233, 'Av Brasil', 'Gravataí','RS');
+
+
+select * from BJ204236.adotante;
+
+INSERT INTO BJ204236.adotante
+VALUES(32826958020,'Pedro Silva Carvalho', '14/12/1990', 7890, 'Rua Marechal Floriano', 'Porto Alegre','RS');
+
+INSERT INTO BJ204236.adotante
+VALUES(17813099060,'Joana Bittecourt Soares', '01/03/1984', 124, 'Av Borges de Medeiros', 'Porto Alegre','RS');
+
+INSERT INTO BJ204236.adotante
+VALUES(13367741000,'Letícia Vilaverde Sardinha', '02/10/1978', 1520, 'Av Coronel Santana', 'Cachoerinha','RS');
+
+INSERT INTO BJ204236.adotante
+VALUES(58833146006,'Martim Lustosa Carregueiro', '25/10/1981', 8555, 'Rua Livramento', 'Viamão','RS');
+
+INSERT INTO BJ204236.adotante
+VALUES(62878465008,'Dario Dourado Feira', '26/09/1984', 992, 'Rua 22 de Outubro', 'Alvorada','RS');
+
+INSERT INTO BJ204236.adotante
+VALUES(79951754007,'Gerson Covilhã Domingos', '15/05/1983', 6542, 'Rua 9 de Abril', 'Porto Alegre','RS');
+
+INSERT INTO BJ204236.adotante
+VALUES(05647413021,'Joyce Marreiro Rangel', '08/06/1989', 541, 'Rua dos Andradas', 'Porto Alegre','RS');
+
+INSERT INTO BJ204236.adotante
+VALUES(50443621071,'Viktoria Cesário Breia', '22/10/1992', 8893, 'Av Bandeirantes', 'Canoas','RS');
+
+INSERT INTO BJ204236.adotante
+VALUES(35383016031,'Kieza Campos Brás', '31/01/1979', 8893, 'Av Cargueiro', 'Sapiranga','RS');
+
+INSERT INTO BJ204236.adotante
+VALUES(79939752040,'Dina Domingues Ávila', '31/03/1985', 323, 'Av Cargueiro', 'Sapiranga','RS');
+
