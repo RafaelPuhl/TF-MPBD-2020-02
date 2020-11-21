d357f1907e614e6a4c563788c06a28b3733720e9
RafaelPuhl <55326343+RafaelPuhl@users.noreply.github.com>
2020-11-21 15:12:11 -0300

    primeiro commit

---
+++ Alters/alter tables fks e sequence .sql
@@ -0,0 +1,68 @@
+alter table processo_adocao
+    add constraint fk_id_forum
+        foreign key (id_forum)
+            references forum (id_forum);
+
+alter table processo_adocao
+    add constraint fk_id_crianca
+        foreign key (id_crianca)
+            references crianca (id_crianca);
+
+alter table processo_adocao
+    add constraint fk_cpf_adotante
+        foreign key (cpf)
+            references adotante (cpf);
+
+alter table telefone_adotante
+    add constraint fk_cpf_telefone
+        foreign key (cpf)
+            references adotante (cpf);
+
+alter table movimentacao_processo
+    add constraint fk_num_processl
+        foreign key (num_processo)
+            references processo_adocao (num_processo);
+
+alter table movimentacao_processo
+    add constraint fk_id_juiz
+        foreign key (id_juiz)
+            references juiz (id_juiz);
+
+alter table historico_adotante
+    add constraint fk_cpf_adotante_historico
+        foreign key (cpf)
+            references adotante (cpf);
+
+alter table historico_crianca
+    add constraint fk_id_crianca_historico
+        foreign key (id_crianca)
+            references crianca (id_crianca);
+
+alter table historico_crianca
+    add constraint fk_cress
+        foreign key (cress)
+            references assistente_social (cress);
+
+alter table historico_adotante
+    add constraint fk_cress_historico_adotante
+        foreign key (cress)
+            references assistente_social (cress);
+--/////////////////////////////////////////////////////////////////
+create sequence seq_criancas;
+create sequence seq_abrigo;
+create sequence seq_juiz;
+create sequence seq_forum;
+
+alter table BJ204236.CRIANCA
+    modify (id_crianca numeric(6) default seq_criancas.nextval);
+
+alter table BJ204236.ABRIGO
+    modify (id_abrigo numeric(4) default seq_abrigo.nextval);
+
+alter table BJ204236.JUIZ
+    modify (id_juiz numeric(6) default seq_juiz.nextval);
+
+alter table BJ204236.FORUM
+    modify (id_forum numeric(4) default seq_criancas.nextval);
+--/////////////////////////////////////////////////////////////////
+commit;
---
+++ Creates/create tables,primary keys e grants.sql
@@ -0,0 +1,195 @@
+-- Create tables e Primary keys
+
+create table adotante
+(
+    cpf             numeric(11)  not null,
+    nome            varchar(100) not null,
+    data_nascimento date         not null,
+    edr_numero      numeric(6)   not null,
+    edr_rua         varchar(100) not null,
+    edr_cidade      varchar(100) not null,
+    edr_uf          varchar(2)   not null
+);
+
+alter table adotante
+    add constraint pk_cpf_adotante
+        primary key (cpf);
+--////////////////////////////////////////////////////////////////
+create table telefone_adotante
+(
+    cpf         numeric(11) not null,
+    id_telefone numeric(12) not null,
+    ddd         numeric(2)  not null,
+    numero      numeric(9)  not null
+);
+
+alter table telefone_adotante
+    add constraint pk_id_telefone
+        primary key (id_telefone);
+--////////////////////////////////////////////////////////////////
+create table psicologo
+(
+    crp    numeric(6)   not null,
+    crp_uf varchar(2)   not null,
+    nome   varchar(100) not null
+);
+
+alter table psicologo
+    add constraint pk_cdp_psicologo
+        primary key (crp);
+--/////////////////////////////////////////////////////////////////
+create table assistente_social
+(
+    cress    numeric(6)   not null,
+    cress_uf varchar(2)   not null,
+    nome     varchar(100) not null
+);
+
+alter table assistente_social
+    add constraint pk_cress_assistente_social
+        primary key (cress);
+--/////////////////////////////////////////////////////////////////
+create table juiz
+(
+    id_juiz numeric(6)   not null,
+    nome    varchar(100) not null
+);
+
+alter table juiz
+    add constraint pk_id_juiz
+        primary key (id_juiz);
+--/////////////////////////////////////////////////////////////////
+create table crianca
+(
+    id_crianca      numeric(6)   not null,
+    nome            varchar(100) not null,
+    data_nascimento date         not null,
+    sexo            char(1)      not null
+);
+
+alter table crianca
+    add constraint pk_id_crianca
+        primary key (id_crianca);
+--/////////////////////////////////////////////////////////////////
+create table forum
+(
+    id_forum   numeric(4)   not null,
+    edr_numero numeric(6)   not null,
+    edr_rua    varchar(100) not null,
+    edr_cidade varchar(100) not null,
+    edr_uf     varchar(2)   not null
+);
+
+alter table forum
+    add constraint pk_id_forum
+        primary key (id_forum);
+--/////////////////////////////////////////////////////////////////
+create table abrigo
+(
+    id_abrigo  numeric(4)   not null,
+    nome       varchar(100) not null,
+    edr_numero numeric(6)   not null,
+    edr_rua    varchar(100) not null,
+    edr_cidade varchar(100) not null,
+    edr_uf     varchar(2)   not null
+);
+
+alter table abrigo
+    add constraint pk_id_abrigo
+        primary key (id_abrigo);
+--/////////////////////////////////////////////////////////////////
+create table historico_crianca
+(
+    id_historico numeric(3)    not null,
+    descricao    varchar(4000) not null,
+    id_crianca   numeric(6)    not null,
+    cress        numeric(6)    not null,
+    cress_uf     varchar(2)    not null
+);
+
+alter table historico_crianca
+    add constraint pk_id_historico_crianca
+        primary key (id_historico);
+--/////////////////////////////////////////////////////////////////
+create table historico_adotante
+(
+    id_historico numeric(3)    not null,
+    descricao    varchar(4000) not null,
+    cpf          numeric(6)    not null,
+    cress        numeric(6)    not null,
+    cress_uf     varchar(2)    not null
+);
+
+alter table historico_adotante
+    add constraint pk_id_historico_adotante
+        primary key (id_historico);
+--/////////////////////////////////////////////////////////////////
+create table movimentacao_processo
+(
+    id_movimentacao numeric(4)    not null,
+    texto_despacho  varchar(4000) not null,
+    num_processo    numeric(20)   not null,
+    id_juiz         numeric(6)    not null
+);
+
+alter table movimentacao_processo
+    add constraint pk_id_movimentacao
+        primary key (id_movimentacao);
+--/////////////////////////////////////////////////////////////////
+create table processo_adocao
+(
+    num_processo numeric(20) not null,
+    id_forum     numeric(4)  not null,
+    tipo         varchar(50) not null,
+    data_inicio  date        not null,
+    data_fim     date,
+    id_crianca   numeric(6)  not null,
+    cpf          numeric(11) not null
+);
+
+alter table processo_adocao
+    add constraint pk_num_processo
+        primary key (num_processo);
+        
+--//////////////////////Liberação de privilegios para os colegas //////////////////////////////////////
+
+grant all privileges on abrigo to BJ103859;
+grant all privileges on adotante to BJ103859;
+grant all privileges on assistente_social to BJ103859;
+grant all privileges on crianca to BJ103859;
+grant all privileges on forum to BJ103859;
+grant all privileges on historico_adotante to BJ103859;
+grant all privileges on historico_crianca to BJ103859;
+grant all privileges on juiz to BJ103859;
+grant all privileges on movimentacao_processo to BJ103859;
+grant all privileges on processo_adocao to BJ103859;
+grant all privileges on psicologo to BJ103859;
+grant all privileges on telefone_adotante to BJ103859;
+
+grant all privileges on abrigo to AF105742;
+grant all privileges on adotante to AF105742;
+grant all privileges on assistente_social to AF105742;
+grant all privileges on crianca to AF105742;
+grant all privileges on forum to AF105742;
+grant all privileges on historico_adotante to AF105742;
+grant all privileges on historico_crianca to AF105742;
+grant all privileges on juiz to AF105742;
+grant all privileges on movimentacao_processo to AF105742;
+grant all privileges on processo_adocao to AF105742;
+grant all privileges on psicologo to AF105742;
+grant all privileges on telefone_adotante to AF105742;
+
+grant all privileges on abrigo to BJ105717;
+grant all privileges on adotante to BJ105717;
+grant all privileges on assistente_social to BJ105717;
+grant all privileges on crianca to BJ105717;
+grant all privileges on forum to BJ105717;
+grant all privileges on historico_adotante to BJ105717;
+grant all privileges on historico_crianca to BJ105717;
+grant all privileges on juiz to BJ105717;
+grant all privileges on movimentacao_processo to BJ105717;
+grant all privileges on processo_adocao to BJ105717;
+grant all privileges on psicologo to BJ105717;
+grant all privileges on telefone_adotante to BJ105717;
+--/////////////////////////////////////////////////////////////////
+commit;
