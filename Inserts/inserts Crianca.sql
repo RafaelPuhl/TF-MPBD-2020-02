62f620da192f51ecfacded372c90cec1900bedf7
Rafael Puhl <rafaeldiaspuhl@gmail.com>
2020-11-21 15:19:16 -0300

    testando repo

---
+++ Inserts/inserts Crianca.sql
@@ -0,0 +1,29 @@
+insert into BJ204236.CRIANCA (NOME, DATA_NASCIMENTO, SEXO)
+values ('Pedrinho Muçun', '10/10/2010', 'M');
+
+insert into BJ204236.CRIANCA (NOME, DATA_NASCIMENTO, SEXO)
+values ('Chuck Norris', '06/06/2006', 'M');
+
+insert into BJ204236.CRIANCA (NOME, DATA_NASCIMENTO, SEXO)
+values ('Anabelle Covas', '25/01/2005', 'F');
+
+insert into BJ204236.CRIANCA (NOME, DATA_NASCIMENTO, SEXO)
+values ('Matheus Carrieri', '24/10/2016', 'M');
+
+insert into BJ204236.CRIANCA (NOME, DATA_NASCIMENTO, SEXO)
+values ('Angus Young', '10/01/2011', 'M');
+
+insert into BJ204236.CRIANCA (NOME, DATA_NASCIMENTO, SEXO)
+values ('Dayse Buckenna', '17/03/2013', 'F');
+
+insert into BJ204236.CRIANCA (NOME, DATA_NASCIMENTO, SEXO)
+values ('Jason Vorhis da Silva', '31/03/2016', 'M');
+
+insert into BJ204236.CRIANCA (NOME, DATA_NASCIMENTO, SEXO)
+values ('Cyndi Lauper', '02/02/2007', 'F');
+
+insert into BJ204236.CRIANCA (NOME, DATA_NASCIMENTO, SEXO)
+values ('Bruce Dickinson', '14/12/2013', 'M');
+
+insert into BJ204236.CRIANCA (NOME, DATA_NASCIMENTO, SEXO)
+values ('Bob Marley', '28/11/2011', 'M');
