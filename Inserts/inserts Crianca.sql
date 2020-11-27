c5f7a662f6d745ed2f86ecaaf050de17ae70a4b5
jonathanCB <jonas.ca2@gmail.com>
2020-11-26 21:32:29 -0300

    Update inserts Crianca.sql

--- Inserts/inserts Crianca.sql
+++ Inserts/inserts Crianca.sql
@@ -1,31 +1,33 @@
-insert into BJ204236.CRIANCA (NOME, DATA_NASCIMENTO, SEXO)
-values ('Pedrinho Muçun', '10/10/2010', 'M');
+insert into BJ204236.CRIANCA (NOME, DATA_NASCIMENTO, SEXO, id_abrigo)
+values ('Pedrinho Muçun', '10/10/2010', 'M', 1);
 
-insert into BJ204236.CRIANCA (NOME, DATA_NASCIMENTO, SEXO)
-values ('Chuck Norris', '06/06/2006', 'M');
+insert into BJ204236.CRIANCA (NOME, DATA_NASCIMENTO, SEXO, id_abrigo)
+values ('Chuck Norris', '06/06/2006', 'M', 1);
 
-insert into BJ204236.CRIANCA (NOME, DATA_NASCIMENTO, SEXO)
-values ('Anabelle Covas', '25/01/2005', 'F');
+insert into BJ204236.CRIANCA (NOME, DATA_NASCIMENTO, SEXO, id_abrigo)
+values ('Anabelle Covas', '25/01/2005', 'F', 1);
 
-insert into BJ204236.CRIANCA (NOME, DATA_NASCIMENTO, SEXO)
-values ('Matheus Carrieri', '24/10/2016', 'M');
+insert into BJ204236.CRIANCA (NOME, DATA_NASCIMENTO, SEXO, id_abrigo)
+values ('Matheus Carrieri', '24/10/2016', 'M', 2);
 
-insert into BJ204236.CRIANCA (NOME, DATA_NASCIMENTO, SEXO)
-values ('Angus Young', '10/01/2011', 'M');
+insert into BJ204236.CRIANCA (NOME, DATA_NASCIMENTO, SEXO, id_abrigo)
+values ('Angus Young', '10/01/2011', 'M', 3);
 
-insert into BJ204236.CRIANCA (NOME, DATA_NASCIMENTO, SEXO)
-values ('Dayse Buckenna', '17/03/2013', 'F');
+insert into BJ204236.CRIANCA (NOME, DATA_NASCIMENTO, SEXO, id_abrigo)
+values ('Dayse Buckenna', '17/03/2013', 'F', 4);
 
-insert into BJ204236.CRIANCA (NOME, DATA_NASCIMENTO, SEXO)
-values ('Jason Vorhis da Silva', '31/03/2016', 'M');
+insert into BJ204236.CRIANCA (NOME, DATA_NASCIMENTO, SEXO, id_abrigo)
+values ('Jason Vorhis da Silva', '31/03/2016', 'M', 5);
 
-insert into BJ204236.CRIANCA (NOME, DATA_NASCIMENTO, SEXO)
-values ('Cyndi Lauper', '02/02/2007', 'F');
+insert into BJ204236.CRIANCA (NOME, DATA_NASCIMENTO, SEXO, id_abrigo)
+values ('Cyndi Lauper', '02/02/2007', 'F', 2);
 
-insert into BJ204236.CRIANCA (NOME, DATA_NASCIMENTO, SEXO)
-values ('Bruce Dickinson', '14/12/2013', 'M');
+insert into BJ204236.CRIANCA (NOME, DATA_NASCIMENTO, SEXO, id_abrigo)
+values ('Bruce Dickinson', '14/12/2013', 'M', 3);
 
-insert into BJ204236.CRIANCA (NOME, DATA_NASCIMENTO, SEXO)
-values ('Bob Marley', '28/11/2011', 'M');
+insert into BJ204236.CRIANCA (NOME, DATA_NASCIMENTO, SEXO, id_abrigo)
+values ('Bob Marley', '28/11/2011', 'M', 4);
+
+select * from BJ204236.crianca;
 
 commit;
