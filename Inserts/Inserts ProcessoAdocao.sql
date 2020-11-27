0b12a3c22d196480c371038ac76d41b7ad801a36
Rafael Puhl <rafaeldiaspuhl@gmail.com>
2020-11-26 22:41:26 -0300

    kkkkkkk

--- Inserts/Inserts ProcessoAdocao.sql
+++ Inserts/Inserts ProcessoAdocao.sql
@@ -1,18 +1,16 @@
-select * from BJ204236.processo_adocao;
-
 insert into BJ204236.processo_adocao (num_processo, id_forum, tipo, data_inicio, data_fim, id_crianca, cpf)
     values ('5788038317142872', 4, 'Tardia', '10/10/2008', '11/03/2019', 5, 79951754007);
-    
-insert into BJ204236.processo_adocao (num_processo, id_forum, tipo, data_inicio, data_fim, id_crianca, cpf)
-    values ('817258408mKl7216', 2, 'Intuitu Personae', '18/07/2012', '08/09/2015', 6, 17813099060);
 
 insert into BJ204236.processo_adocao (num_processo, id_forum, tipo, data_inicio, data_fim, id_crianca, cpf)
-    values ('30042428cGN88641', 1, 'Homopaternal', '19/06/201', '08/09/2016', 7, 13367741000);
-    
+    values ('8172584089877216', 2, 'Intuitu Personae', '18/07/2012', '08/09/2015', 6, 17813099060);
+
 insert into BJ204236.processo_adocao (num_processo, id_forum, tipo, data_inicio, data_fim, id_crianca, cpf)
-    values ('532851587iOg2668', 3, 'Brasileira', '10/10/2010', null, 2, 32826958020);
-    
+    values ('3004242824188641', 1, 'Homopaternal', '19/06/2011', '08/09/2016', 7, 13367741000);
+
 insert into BJ204236.processo_adocao (num_processo, id_forum, tipo, data_inicio, data_fim, id_crianca, cpf)
-    values ('6848109107545oCq1817', 5, 'Por Testamento', '25/10/2013', null, 1, 79951754007);
-    
+    values ('5328515870142668', 3, 'Brasileira', '10/10/2010', null, 2, 32826958020);
+
+insert into BJ204236.processo_adocao (num_processo, id_forum, tipo, data_inicio, data_fim, id_crianca, cpf)
+    values ('6848175459341817', 5, 'Por Testamento', '25/10/2013', null, 1, 35383016031);
+
 commit;
